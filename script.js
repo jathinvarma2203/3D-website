ef4abc75c8c2c7bd86545aa0a9098200a78e7672
jathinvarma2203 <vanapalacaitanya@gmail.com>
2024-07-01 16:41:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 document.addEventListener('keydown', function(event) {
-  const tree = document.querySelector('[gltf-model="#treeModel"]').body;
-  const house = document.querySelector('[gltf-model="#houseModel"]').body;
-  const torus = document.querySelector('a-torus').body;
-  const dodecahedron = document.querySelector('a-dodecahedron').body;
+  const ball = document.querySelector('#ball').body;
 
   const applyForce = (body, forceVec) => {
     body.applyForce(new CANNON.Vec3(...forceVec), new CANNON.Vec3().copy(body.position));
@@ -10,28 +7,16 @@ document.addEventListener('keydown', function(event) {
 
   switch (event.key) {
     case 'ArrowUp':
-      applyForce(tree, [0, 0, -5]);
-      applyForce(house, [0, 0, -5]);
-      applyForce(torus, [0, 0, -5]);
-      applyForce(dodecahedron, [0, 0, -5]);
+      applyForce(ball, [0, 0, -10]);
       break;
     case 'ArrowDown':
-      applyForce(tree, [0, 0, 5]);
-      applyForce(house, [0, 0, 5]);
-      applyForce(torus, [0, 0, 5]);
-      applyForce(dodecahedron, [0, 0, 5]);
+      applyForce(ball, [0, 0, 10]);
       break;
     case 'ArrowLeft':
-      applyForce(tree, [-5, 0, 0]);
-      applyForce(house, [-5, 0, 0]);
-      applyForce(torus, [-5, 0, 0]);
-      applyForce(dodecahedron, [-5, 0, 0]);
+      applyForce(ball, [-10, 0, 0]);
       break;
     case 'ArrowRight':
-      applyForce(tree, [5, 0, 0]);
-      applyForce(house, [5, 0, 0]);
-      applyForce(torus, [5, 0, 0]);
-      applyForce(dodecahedron, [5, 0, 0]);
+      applyForce(ball, [10, 0, 0]);
       break;
   }
 });
